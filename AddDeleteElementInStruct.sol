5d32f387680f9f4d0d73424c3672403a94f6ae22
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-20 13:15:15 +0300

    Update AddDeleteElementInStruct.sol

--- AddDeleteElementInStruct.sol
+++ AddDeleteElementInStruct.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
-/// @title Voting with delegation.
+/// @title Add & Delete Element In Struct
 
 contract workingWithStructs {
     
