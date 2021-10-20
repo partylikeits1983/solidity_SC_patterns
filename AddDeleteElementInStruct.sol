d920b7471827572182aaf9007b9771221f1e867c
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-20 13:14:41 +0300

    Create AddDeleteElementInStruct.sol

---
+++ AddDeleteElementInStruct.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+/// @title Voting with delegation.
+
+contract workingWithStructs {
+    
+    struct data {  
+        address account;
+        uint name;  
+        uint nickname;  
+    }
+    
+    mapping(address => data) public USER;
+    
+    address[] public users;
+
+    
+    function add(address user) public {
+        // push user address to array
+        users.push(user);
+        
+        //a dd random data to USER struct
+        USER[msg.sender].account = msg.sender;
+        USER[msg.sender].name = 1;
+        USER[msg.sender].nickname = block.timestamp;
+    }
+    
+    
+    function remove(address user) public {
+        // remove sinlge element from mapping 
+        delete USER[user];
+        
+    }
+    
+    
+    function removeAll() public {
+        // since you can't delete an entire mapping in solidity
+        // you can reset the struct address by address
+        
+        // for address in array users, delete element in USER
+        for (uint i = 0; i < users.length; i++)
+            
+            // delete the element by user address
+            delete USER[users[i]];
+        
+        // finally, delete the array
+        delete users;
+    }
+}
+
