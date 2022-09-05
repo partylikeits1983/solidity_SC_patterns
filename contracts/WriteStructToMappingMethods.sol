b196b40d9e217469caeb41fd1135a1495d48c91e
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2022-09-05 15:44:46 +0300

    Create WriteStructToMappingMethods.sol

---
+++ contracts/WriteStructToMappingMethods.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract StructTest {
+
+    struct StructA {
+        address tokenA;
+        address tokenB;
+        uint tokenA_balance;
+        uint tokenB_balance;
+        bool isBool;
+        StructB structB;
+    }
+
+    struct StructB {
+        uint a;
+        uint b;
+        uint c;
+    }
+
+    mapping(address => mapping(address => mapping(uint => StructA))) public MappingA;
+
+    function write_mapping(address pair, address user, uint ID, StructA memory _structA) public {
+        MappingA[pair][user][ID] = _structA;
+    }
+
+    function write_mapping2(address pair, address user, uint ID, StructA memory _structA) public {
+        MappingA[pair][user][ID].tokenA = _structA.tokenA;
+        MappingA[pair][user][ID].tokenB = _structA.tokenB;
+        MappingA[pair][user][ID].tokenA_balance = _structA.tokenA_balance;
+        MappingA[pair][user][ID].tokenB_balance = _structA.tokenB_balance;
+
+        MappingA[pair][user][ID].isBool = _structA.isBool;
+        // @dev nested struct params
+        MappingA[pair][user][ID].structB.a = _structA.structB.a;
+        MappingA[pair][user][ID].structB.b = _structA.structB.a;
+        MappingA[pair][user][ID].structB.c = _structA.structB.a;
+    }
+}
